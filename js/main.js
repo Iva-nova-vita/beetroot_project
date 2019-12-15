adf9afd55b32b97ed7827ada0f411dfeba7acb61
iva-nova-vita <ivanovavita89@gmail.com>
2019-12-15 21:15:32 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(function() {
+    /**
+     * active link in nav
+     */
+
+
+    /*let pathname_url = window.location.pathname;
+    let href_url = window.location.href;
+
+    $("nav li").each(function () {
+
+        let link = $(this).find("a").attr("href");
+
+        if(pathname_url == link || href_url == link) {
+
+            $(this).addClass("active");
+
+        }
+
+    });*/
+
+
+    $('#popGoodsSlider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
+      });
+          
+});
+
