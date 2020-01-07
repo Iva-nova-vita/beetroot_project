a73246598073cac8978db01b5ae2315b827dba3f
iva-nova-vita <ivanovavita89@gmail.com>
2020-01-07 13:39:28 +0200

    display of goods on the page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+function init() {
+  //вычитуем файл goods.json
+  $.getJSON("../goods/goods.json", goodsOut);
+}
+
+function goodsOut(data) {
+  // вывод товаров на страницу
+  console.log(data);
+  let out='';
+  for (let key in data) {
+     
+      out +='<div class="productItem">';
+      out +=`<img src="../images/${data[key].img}" alt="">`;
+      out +=`<p>${data[key].name}</p>`;
+      out +=`<span>${data[key].brend}</span>`;
+      out +=`<h3>${data[key].description}</h3>`;
+      out +=`<div><span>${data[key].cost} грн</span>`;
+      out +=`<button><img src="../images/heart.png" alt="heart" /></button>`;
+      out +='<button class="addToCart"><img src="../images/VectorCart.png" alt="shopping-bag" /></button>';
+      out +='</div></div>';
+  }
+  $('.goods-out').html(out);
+}
 $(function() {
   /**
    * active link in nav
@@ -17,10 +40,13 @@ $(function() {
         }
 
     });*/
+    init();
 
   $("#popGoodsSlider").slick({
     infinite: true,
     slidesToShow: 3,
+    
+
     slidesToScroll: 1
   });
 
@@ -30,6 +56,9 @@ $(function() {
     slidesToScroll: 1,
     arrows: false,
     dots: true,
+    autoplay: true,
+
+  autoplaySpeed: 3000,
  
   });
 
